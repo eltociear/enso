dd1da1136044d3732c096352f2a5b166c10e4b2d
Adam Obuchowicz <adam.obuchowicz@enso.org>
2024-02-02 15:39:18 +0100

    Wait for the nodes

--- app/gui2/e2e/actions.ts
+++ app/gui2/e2e/actions.ts
@@ -1,4 +1,6 @@
 import { expect, type Page } from '@playwright/test'
+import * as customExpect from './customExpect'
+import * as locate from './locate'
 
 // =================
 // === goToGraph ===
@@ -7,4 +9,6 @@ import { expect, type Page } from '@playwright/test'
 export async function goToGraph(page: Page) {
   await page.goto('/')
   expect(page.locator('.App')).toBeVisible()
+  // Wait until nodes are loaded.
+  customExpect.toExist(locate.graphNode(page))
 }
